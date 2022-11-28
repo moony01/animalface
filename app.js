c5f07632674e597da1b8eff612ec573f4f26a867
mhhan <mun01180@gmail.com>
2022-11-28 16:47:56 +0900

    123

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('./index.html')
+// })
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use(express.static("views"));
